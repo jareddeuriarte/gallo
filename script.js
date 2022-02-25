5e5d1d2d30f2bf561f5e9b1c5b190427c3b9a3d3
Jared de Uriarte <jared.deuri@gmail.com>
2022-02-25 14:31:34 -0500

    slick working on the page

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-const splash = document.querySelector('.splash')
-
-document.addEventListener('DOMContentLoaded', (e) => {
-
-    console.log('loaded')
-    setTimeout(() => {
-        splash.classList.add('display-none');
-    }, 3000);
 
 
-})
+console.log('hello world')
 
 
-// with bttn option 
-// const bttn = document.querySelector('.bttn')
-
-// bttn.addEventListener('clicl', (e) => {
-
-//     splash.classList.remove('splash');
-//     splash.classList.add('box-transition');
-//     splash.classList.add('box-hidden');
 
 
-// })
+$('.slider').slick({
+  // dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+
+
+
+
+
